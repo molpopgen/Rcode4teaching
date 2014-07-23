a9f95e1d45865119af30f95c1cf00b38b7c9ecd6
Kevin R Thornton <krthornt@thornton01.bio.uci.edu>
2014-07-23 14:59:37 -0700

    doc and code cleanup

--- R/WF.R
+++ R/WF.R
@@ -24,26 +24,7 @@ WFtrajectory <- function(N)
 #' @export
 WFgenic <- function(N,s)
 {
-	wAA = 1+2*s
-	wAa = 1+s
-	waa = 1
-
-	p = 1/(2*N)
-	x = 1
-	traj <- array();
-	generation = 1
-	traj[generation] = p
-	while(p > 0 && p < 1)
-	{
-		q = 1-p
-		wbar = p*p*wAA + 2*p*q*wAa + q*q*waa
-		epprime = (p*p*wAA + p*q*wAa)/wbar
-		x = rbinom(1,2*N,epprime)
-		p = x/(2*N)
-		generation = generation+1
-		traj[generation] = p
-	}
-	return(traj)
+  return( rtraj(1/(2*N),N,1,1+s,1+2*s) )
 }
 
 #' Trajectory of a new  mutation subject to selection with arbitrary dominance in a diploid Wright-Fisher population of size N
@@ -54,60 +35,22 @@ WFgenic <- function(N,s)
 #' @export
 WFdominance <- function(N,s,h)
 {
-	wAA = 1+s
-	wAa = 1+s*h
-	waa = 1
-
-	p = 1/(2*N)
-	x = 1
-	traj <- array();
-	generation = 1
-	traj[generation] = p
-	while(p > 0 && p < 1)
-	{
-		q = 1-p
-		wbar = p*p*wAA + 2*p*q*wAa + q*q*waa
-		epprime = (p*p*wAA + p*q*wAa)/wbar
-		x = rbinom(1,2*N,epprime)
-		p = x/(2*N)
-		generation = generation+1
-		traj[generation] = p
-	}
-	return(traj)
+  return ( rtraj(1/(2*N),N,1,1+s*h,1+2*s) )
 }
 
 #' Trajectory of a new  mutation subject to overdomimant selection in a diploid Wright-Fisher population of size N
 #' @param N The diploid population size
-#' @param s1 The selection coefficient applied to Aa
-#' @param s2 The selection coefficient applied to Aa
+#' @param s1 The selection coefficient applied to AA
+#' @param s2 The selection coefficient applied to aa
 #' @param h The dominance of the mutant allele
 #' @return The allele frequency in each generation until fixation or loss
 #' @export
 WFoverdominance <- function(N,s1,s2,ngen,ifreq)
 {
-	wAA = 1-s2
-	wAa = 1
-	waa = 1-s1
-
-	p = ifreq
-	x = as.integer(ifreq*2*N)
-	traj <- array();
-	generation = 1
-	traj[generation] = p
-	while(generation < ngen)
-	{
-		q = 1-p
-		wbar = p*p*wAA + 2*p*q*wAa + q*q*waa
-		epprime = (p*p*wAA + p*q*wAa)/wbar
-		x = rbinom(1,2*N,epprime)
-		p = x/(2*N)
-		generation = generation+1
-		traj[generation] = p
-	}
-	return(traj)
+  return ( rtraj(1/(2*N),N,1-s2,1,1-s1) )
 }
 
-#' Estimate fixation probability under genic/additive selection
+#' Estimate fixation probability of a new mutation under genic/additive selection
 #' @param N The diploid population size
 #' @param s The selection coefficient
 #' @param howmany The number of simulation to run
@@ -115,21 +58,22 @@ WFoverdominance <- function(N,s1,s2,ngen,ifreq)
 #' @export
 pfixgenic <- function(N,s,howmany)
 {
-	i=0
-	nfix = 0;
-	while( i < howmany )
-	{
-		i=i+1
-		t<-WFgenic(N,s)
-		if ( t[length(t)] == 1 )
-		{
-			nfix=nfix+1
-		}
-	}
-	return(nfix/howmany)
+  return( pfix(1/(2*N),N,1,1+s,1+2*s,howmany) )
+  ## i=0
+  ## nfix = 0;
+  ## while( i < howmany )
+  ## {
+  ## 	i=i+1
+  ## 	t<-WFgenic(N,s)
+  ## 	if ( t[length(t)] == 1 )
+  ## 	{
+  ## 		nfix=nfix+1
+  ## 	}
+  ## }
+  ## return(nfix/howmany)
 }
 
-#' Estimate fixation probability under models with arbitrary dominance
+#' Estimate fixation probability of a new mutation under models with arbitrary dominance
 #' @param N The diploid population size
 #' @param s The selection coefficient
 #' @param h The domimance of the mutant allele
@@ -138,18 +82,19 @@ pfixgenic <- function(N,s,howmany)
 #' @export
 pfixdominance <- function(N,s,h,howmany)
 {
-	i=0
-	nfix = 0;
-	while( i < howmany )
-	{
-		i=i+1
-		t<-WFdominance(N,s,h)
-		if ( t[length(t)] == 1 )
-		{
-			nfix=nfix+1
-		}
-	}
-	return(nfix/howmany)
+  return( pfix(1/(2*N),N,1,1+s*h,1+2*s,howmany) )
+	## i=0
+	## nfix = 0;
+	## while( i < howmany )
+	## {
+	## 	i=i+1
+        ##         t<-WFdominance(N,s,h)
+	## 	if ( t[length(t)] == 1 )
+	## 	{
+	## 		nfix=nfix+1
+	## 	}
+	## }
+	## return(nfix/howmany)
 }
 
 #' Returns a trajectory of a mutation that has fixed due to selection
